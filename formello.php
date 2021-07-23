3db3c7d404aa3b1c12973a8567633f58fe450ea2
Tropicalista <francescopepe@email.it>
2021-07-23 11:20:56 +0200

    bump

--- formello.php
+++ formello.php
@@ -3,7 +3,7 @@
  * Plugin Name: Formello
  * Plugin URI: https://formello.net/
  * Description: Lightweight Gutenberg contact form builder, blazingly fast with minnimal external dependencies and ReCaptcha support.
- * Version: 1.2.1
+ * Version: 1.2.2
  * Author: Formello
  * Author URI: https://formello.net
  * License: GPL2
@@ -29,7 +29,7 @@ final class Formello {
 	 *
 	 * @var string
 	 */
-	public $version = '1.2.1';
+	public $version = '1.2.2';
 
 	/**
 	 * Holds various class instances
