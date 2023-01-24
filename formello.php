bb82577b9d8d3696fc06ee9adf35f1bf7cfc2a1c
Tropicalista <francescopepe@email.it>
2023-01-24 16:22:47 +0100

    test

--- formello.php
+++ formello.php
@@ -3,7 +3,7 @@
  * Plugin Name: Formello
  * Plugin URI: https://formello.net/
  * Description: Lightweight Gutenberg contact form builder, blazingly fast with minnimal external dependencies and ReCaptcha support.
- * Version: 1.9.0
+ * Version: 1.9.1
  * Author: Formello
  * Author URI: https://formello.net
  * License: GPL2
