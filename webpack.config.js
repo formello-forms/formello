4ff6beb92dfef4458dfd0dd9392e4ea1a5d6545e
Tropicalista <francescopepe@email.it>
2023-04-17 23:45:12 +0200

    bump

--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,5 @@ module.exports = {
 		settings: [ path.resolve( __dirname, 'src', 'settings', 'index.js' ) ],
 		submission: [ path.resolve( __dirname, 'src', 'submission', 'index.js' ) ],
 		frontend: [ path.resolve( __dirname, 'src', 'frontend', 'frontend.js' ) ],
-	},
-	resolve: {
-		alias: {
-			//Components: path.resolve( __dirname, 'src/components/' ),
-		},
-	},
+	}
 };
