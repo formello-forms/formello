879add28ac8836c87c345012a5cae8d6dc56b4ea
Tropicalista <francescopepe@email.it>
2024-10-23 08:08:50 +0200

    remove dev from vendor

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,9 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
-    '38fbb71d514eee25044aa9d94851ee76' => $vendorDir . '/wp-phpunit/wp-phpunit/__loaded.php',
-    '7d3b315c4f303f2fc14aca642a738e50' => $vendorDir . '/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php',
     '0d73450dcae6c75c8ae55e881f84b8fd' => $baseDir . '/includes/Utils/templates.php',
     '518f2fdfcbdb55daf7b7cf17bf9eb26a' => $baseDir . '/includes/Utils/functions.php',
     '58b6f653b973bcb18404f2deee61cd27' => $baseDir . '/includes/Utils/register-cpt.php',
