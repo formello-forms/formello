987721cc961c249ec3e13bce18ca706eb1b4d308
Tropicalista <francescopepe@email.it>
2025-02-10 19:55:24 +0100

    add ip logging to admin

--- src/admin/pages/settings/components/general.js
+++ src/admin/pages/settings/components/general.js
@@ -6,6 +6,7 @@ import {
 	Button,
 	__experimentalInputControl as InputControl,
 	Notice,
+	ToggleControl,
 } from '@wordpress/components';
 
 import {
@@ -51,95 +52,124 @@ export default function General() {
 	};
 
 	return (
-		<Card>
-			<CardHeader>
-				<h2>{ __( 'License', 'formello' ) }</h2>
-			</CardHeader>
+		<Fragment>
+			<Card>
+				<CardHeader>
+					<h2>{ __( 'License', 'formello' ) }</h2>
+				</CardHeader>
 
-			<CardBody>
-				<ExternalLink href="https://formello.net">
-					{ __( 'Get a PRO License', 'formello' ) }
-				</ExternalLink>
-				<RawHTML>
-					{ __(
-						'<p>Your <strong>pro license</strong> key provides access to addons. You can still using Formello without a license key.</p>',
-						'formello'
+				<CardBody>
+					<ExternalLink href="https://formello.net">
+						{ __( 'Get a PRO License', 'formello' ) }
+					</ExternalLink>
+					<RawHTML>
+						{ __(
+							'<p>Your <strong>pro license</strong> key provides access to addons. You can still using Formello without a license key.</p>',
+							'formello'
+						) }
+					</RawHTML>
+					<InputControl
+						type="text"
+						autoComplete="new-password"
+						label={ __( 'License Key', 'formello' ) }
+						value={ licenseKey.current }
+						onChange={ ( val ) => ( licenseKey.current = val ) }
+						suffix={
+							<Fragment>
+								<Button
+									isBusy={ loading }
+									aria-disabled={ loading }
+									disabled={ loading }
+									text={ __( 'check', 'formello' ) }
+									onClick={ () =>
+										updateLicense( 'validate' )
+									}
+								/>
+								<Button
+									variant="primary"
+									isBusy={ loading }
+									aria-disabled={
+										loading || '' === licenseKey
+									}
+									disabled={ loading || '' === licenseKey }
+									text={ __( 'Activate', 'formello' ) }
+									onClick={ () => updateLicense() }
+								/>
+							</Fragment>
+						}
+						__next40pxDefaultSize
+					/>
+					{ message && (
+						<Notice
+							isDismissible={ false }
+							className={
+								'active' ===
+								settings.license?.license_key?.status
+									? 'success'
+									: 'warning'
+							}
+							status={
+								'active' ===
+								settings.license?.license_key?.status
+									? 'success'
+									: 'warning'
+							}
+						>
+							<RawHTML>{ message }</RawHTML>
+						</Notice>
 					) }
-				</RawHTML>
-				<InputControl
-					type="text"
-					autoComplete="new-password"
-					label={ __( 'License Key', 'formello' ) }
-					value={ licenseKey.current }
-					onChange={ ( val ) => ( licenseKey.current = val ) }
-					suffix={
-						<Fragment>
-							<Button
-								isBusy={ loading }
-								aria-disabled={ loading }
-								disabled={ loading }
-								text={ __( 'check', 'formello' ) }
-								onClick={ () => updateLicense( 'validate' ) }
-							/>
-							<Button
-								variant="primary"
-								isBusy={ loading }
-								aria-disabled={ loading || '' === licenseKey }
-								disabled={ loading || '' === licenseKey }
-								text={ __( 'Activate', 'formello' ) }
-								onClick={ () => updateLicense() }
-							/>
-						</Fragment>
-					}
-					__next40pxDefaultSize
-				/>
-				{ message && (
-					<Notice
-						isDismissible={ false }
-						className={
-							'active' === settings.license?.license_key?.status
-								? 'success'
-								: 'warning'
+					{ settings?.license?.license_key?.status && (
+						<Notice
+							isDismissible={ false }
+							status={
+								'active' ===
+								settings.license?.license_key?.status
+									? 'success'
+									: 'warning'
+							}
+						>
+							<RawHTML>
+								{ sprintf(
+									/* translators: %s: License status. */
+									__(
+										'<p>License status: <b>%1$s</b>. Expires: %2$s</p>',
+										'formello'
+									),
+									settings.license.license_key.status,
+									__( 'Never', 'tropical-juice' ) !==
+										settings.license.license_key.expires_at
+										? dateI18n(
+												'd M, Y',
+												settings.license.license_key
+													.expires_at
+										  )
+										: __( 'Never', 'tropical-juice' )
+								) }
+							</RawHTML>
+						</Notice>
+					) }
+				</CardBody>
+			</Card>
+			<Card>
+				<CardHeader>
+					<h2>{ __( 'Ip logging', 'formello' ) }</h2>
+				</CardHeader>
+
+				<CardBody>
+					<ToggleControl
+						label={ __( 'Enable IP logging', 'formello' ) }
+						checked={ settings.ip_logging }
+						onChange={ ( val ) =>
+							updateSetting( 'ip_logging', val )
 						}
-						status={
-							'active' === settings.license?.license_key?.status
-								? 'success'
-								: 'warning'
-						}
-					>
-						<RawHTML>{ message }</RawHTML>
-					</Notice>
-				) }
-				{ settings?.license?.license_key?.status && (
-					<Notice
-						isDismissible={ false }
-						status={
-							'active' === settings.license?.license_key?.status
-								? 'success'
-								: 'warning'
-						}
-					>
-						<RawHTML>
-							{ sprintf(
-								/* translators: %s: License status. */
-								__(
-									'<p>License status: <b>%1$s</b>. Expires: %2$s</p>',
-									'formello'
-								),
-								settings.license.license_key.status,
-								__( 'Never', 'tropical-juice' ) !==
-									settings.license.license_key.expires_at
-									? dateI18n(
-											'd M, Y',
-											settings.license.license_key
-												.expires_at
-									  )
-									: __( 'Never', 'tropical-juice' )
-							) }
-						</RawHTML>
-					</Notice>
-				) }
-			</CardBody>
-		</Card>
+						help={ __(
+							"If this option is turned on, the user's IP address will be saved with the form data",
+							'formello'
+						) }
+						__nextHasNoMarginBottom
+					/>
+				</CardBody>
+			</Card>
+		</Fragment>
 	);
 }
