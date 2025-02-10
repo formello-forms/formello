5e79abfc0d352aec0db07883708569f15d20dcc9
Tropicalista <francescopepe@email.it>
2025-02-10 19:51:44 +0100

    added settings endpoint

---
+++ includes/Rest/Settings.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Manage Settings.
+ *
+ * @package Formello
+ */
+
+namespace Formello\Rest;
+
+/**
+ * REST_API Handler
+ */
+class Settings extends Base {
+
+	/**
+	 * {inheritDoc}
+	 */
+	public function __construct() {
+		$this->rest_base = 'settings';
+	}
+
+	/**
+	 * {inheritDoc}
+	 */
+	public function endpoints() {
+		return array(
+			'/reset' => array(
+				'methods' => \WP_REST_Server::CREATABLE,
+				'callback' => array( $this, 'reset' ),
+				'permission_callback' => array( $this, 'update_settings_permissions' ),
+			),
+		);
+	}
+
+	/**
+	 * Get all registered columns.
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 * @return Array
+	 */
+	public function reset( $request ) {
+		delete_option( 'formello' );
+
+		return array(
+			'success' => true,
+			'response' => __( 'Settings resetted!', 'formello' ),
+		);
+	}
+}
