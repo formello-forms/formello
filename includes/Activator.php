345f5d5844ce654ea87f0e64814b469be175aacf
Tropicalista <francescopepe@email.it>
2025-02-12 17:26:53 +0100

    added migration

--- includes/Activator.php
+++ includes/Activator.php
@@ -22,9 +22,49 @@ namespace Formello;
  * @author     Francesco Pepe <sgozzapolli@gmail.com>
  */
 class Activator {
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    2.6.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
 
 	/**
-	 * Short Description. (use period)
+	 * The version of this plugin.
+	 *
+	 * @since    2.6.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * The main file of this plugin.
+	 *
+	 * @since    2.6.0
+	 * @access   private
+	 * @var      string    $version    The main file of this plugin.
+	 */
+	private $entry_point;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since 2.6.0
+	 * @param string $plugin_name The name of this plugin.
+	 * @param string $version     The version of this plugin.
+	 * @param string $entry_point The main file of this plugin.
+	 */
+	public function __construct( $plugin_name, $version, $entry_point ) {
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+		$this->entry_point = $entry_point;
+	}
+
+	/**
+	 * Code to run after activation.
 	 *
 	 * Long Description.
 	 *
@@ -61,7 +101,7 @@ class Activator {
 			) ENGINE=InnoDB CHARACTER SET={$wpdb->charset};"
 		);
 
-		$wpdb->query( "UPDATE {$wpdb->prefix}posts SET post_type = 'formello' WHERE post_type = 'formello_form';" );
+		$wpdb->query( "UPDATE {$wpdb->prefix}posts SET post_type = 'formello_form' WHERE post_type = 'formello';" );
 
 		$upload_dir   = wp_upload_dir();
 		$formello_dir = $upload_dir['basedir'] . '/formello';
@@ -87,5 +127,61 @@ class Activator {
 
 		update_option( 'formello_version', get_file_data( __FILE__, array( 'Version' ) )[0] );
 		update_option( 'formello_installed', time() );
+	}
+
+	/**
+	 * COde to run after plugin update.
+	 *
+	 * @param [type] $upgrader_object
+	 * @param [type] $options
+	 * @return void
+	 */
+	public function update_completed( $upgrader_object, $options ) {
+
+		// If an update has taken place and the updated type is plugins and the plugins element exists.
+		if ( 'update' === $options['action'] && 'plugin' === $options['type'] && isset( $options['plugins'] ) ) {
+			foreach ( $options['plugins'] as $plugin ) {
+				// Check to ensure it's my plugin.
+				if ( plugin_basename( $this->entry_point ) === $plugin ) {
+					$this->add_message();
+				}
+			}
+		}
+	}
+
+	/**
+	 * Add message block.
+	 *
+	 * @return void
+	 */
+	public function add_message() {
+		$forms = get_posts(
+			array(
+				'post_type'   => 'formello_form',
+				'post_status' => 'publish',
+				'numberposts' => -1,
+			)
+		);
+
+		foreach ( $forms as $key => $form ) {
+
+			if ( ! has_block( 'formello/message', $form->post_content ) ) {
+
+				$updated_content = str_replace(
+					'</form>',
+					'<!-- wp:formello/message -->
+<div class="wp-block-formello-message" data-wp-class--success="context.response.success" data-wp-class--error="!context.response.success" data-wp-interactive="formello" data-wp-context="{}"><p data-wp-text="state.message"></p><ul data-wp-context="state.errors"><template data-wp-each="state.errors"><li data-wp-text="context.item"></li></template></ul></div>
+<!-- /wp:formello/message --></form>',
+					$form->post_content
+				);
+
+				$updated_form = array(
+					'ID'           => $form->ID,
+					'post_content' => $updated_content,
+				);
+				wp_update_post( $updated_form, false, false );
+				wp_reset_postdata();
+			}
+		}
 	}
 }
